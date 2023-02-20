4e8fa97d4bd5a61a54d79046342ef14d1f3c8b00
abdybekusyimyk <abdybekuulusyimyk@gmail.com>
2023-02-20 19:15:18 +0600

    locash

--- weatherlogiv/lib/home_page.dart
+++ weatherlogiv/lib/home_page.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weatherlogiv/components/cutom_button.dart';
 import 'package:weatherlogiv/constans/api.dart';
 import 'package:weatherlogiv/constans/app_text_style.dart';
@@ -17,13 +18,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<Weather?> fetchData() async {
+  Future<void> weatherLoction() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        await fetchData();
+      }
+    } else {
+      Position position = await Geolocator.getCurrentPosition();
+      print(position.latitude);
+      print(position.longitude);
+    }
+  }
+
+  Future<Weather?>? fetchData() async {
     final dio = Dio();
     final response = await dio.get(ApiConst.api);
     print(response);
     if (response.statusCode == 200) {
       final Weather weather = Weather(
-          main: response.data['weather'][0]['main'],
+          temp: response.data['main']['temp'],
           description: response.data['weather'][0]['description'],
           icon: response.data['weather'][0]['icon'],
           name: response.data['name']);
@@ -45,76 +61,87 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/weather.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomButton(
-                  icon: Icons.near_me,
+      body: FutureBuilder<Weather?>(
+        future: fetchData(),
+        builder: (cnt, sn) {
+          if (sn.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (sn.connectionState == ConnectionState.none) {
+            return Text("iternete bir katabar kaira kirip kor");
+          } else if (sn.connectionState == ConnectionState.done) {
+            if (sn.hasError) {
+              return Text("${sn.error}");
+            } else if (sn.hasData) {
+              final weather = sn.data!;
+
+              return Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/weather.jpg'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                CustomButton(icon: Icons.location_city),
-              ],
-            ),
-            Row(
-              children: [
-                const SizedBox(width: 20),
-                const Text(
-                  '8',
-                  style: AppTextStyle.temp,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomButton(
+                          onPressed: () async {
+                            await weatherLoction();
+                          },
+                          icon: Icons.near_me,
+                        ),
+                        CustomButton(
+                            onPressed: () {}, icon: Icons.location_city),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const SizedBox(width: 20),
+                        Text(
+                          "${(weather.temp - 273.15).truncate()}",
+                          style: AppTextStyle.temp,
+                        ),
+                        Image.network(ApiConst.getIcon(weather.icon, 4))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: FittedBox(
+                        child: Text(
+                          weather.description.replaceAll(' ', '\n'),
+                          // "You'all need and ".replaceAll(' ', '\n'),
+                          style: AppTextStyle.centertitle,
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: FittedBox(
+                        child: Text(
+                          weather.name,
+                          style: AppTextStyle.centertitle,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                Image.network(ApiConst.getIcon('11n', 4))
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  "You'all need and ".replaceAll(' ', '\n'),
-                  style: AppTextStyle.centertitle,
-                  textAlign: TextAlign.right,
-                ),
-              ],
-            ),
-            Text(
-              "Bishkek",
-              style: AppTextStyle.centertitle,
-            )
-          ],
-        ),
+              );
+            } else {
+              return Text("Belgisiz kata");
+            }
+          } else {
+            return Text("Belgisiz kata");
+          }
+        },
       ),
-      // body: Center(
-      //   child: FutureBuilder(
-      //     future: fetchData(),
-      //     builder: (ctx, sn) {
-      //       if (sn.hasData) {
-      //         return Column(
-      //           mainAxisAlignment: MainAxisAlignment.center,
-      //           children: [
-      //             Text(
-      //               sn.data!.main,
-      //               style: TextStyle(fontSize: 30),
-      //             ),
-      //             Text(sn.data!.description),
-      //             Text(sn.data!.icon),
-      //             Text(sn.data!.name),
-      //           ],
-      //         );
-      //       } else if (sn.hasError) {
-      //         return Text(sn.error.toString());
-      //       } else {
-      //         return const CircularProgressIndicator();
-      //       }
-      //     },
-      //   ),
-      // ),
     );
   }
 }
